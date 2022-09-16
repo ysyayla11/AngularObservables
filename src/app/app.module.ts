04945c0090c0a4c1047dc315b6614b3881c9317c
yavuzsy11 <yavuz-selim.yayla@capgemini.com>
2022-09-16 14:42:36 +0200

    dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TextboxComponent } from './textbox/textbox.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
 
 
 
@@ -26,7 +28,9 @@ import { TextboxComponent } from './textbox/textbox.component';
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule, 
+    ReactiveFormsModule,
+    BrowserAnimationsModule, 
+    MatDialogModule
     
   ],
   providers: [],
--- src/app/textbox/textbox.component.ts
+++ src/app/textbox/textbox.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit,} from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 import { ObservorService } from '../observor.service';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 
 @Component({
@@ -11,10 +11,23 @@ import { ObservorService } from '../observor.service';
 })
 export class TextboxComponent implements OnInit {
   
+  
   //Construct the service, do we need a new name here?
   //Is it assigned to a variable? Why do we have this in 
   //the constructor but not under oninit?
-  constructor(private UsernameService: ObservorService) { 
+  constructor(private UsernameService: ObservorService, public dialog: MatDialog) { 
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialog, {
+      width: '250px',
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      
+    });
+  
+  
   }
 
   public searchControl: FormControl = new FormControl('');
@@ -27,4 +40,19 @@ export class TextboxComponent implements OnInit {
   };
       
     
+}
+
+@Component({
+  selector: 'dialog-overview-example-dialog',
+  template: `<h1 mat-dialog-title>Hi</h1>`
+ 
+})
+export class DialogOverviewExampleDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DialogOverviewExampleDialog>
+  ) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
 }
